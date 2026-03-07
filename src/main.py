a8ed82dbc8e8f7970f66d6ea3c5423a2ca40de6c
son-sai <sonsai.forwork@gmail.com>
2026-03-07 14:08:08 +0900

    更新参数获取方式

---
+++ src/main.py
@@ -0,0 +1,16 @@
+import json
+import os
+import sys
+from utility import show_kvk_match_data
+
+mode = os.environ["MODE"]
+
+if mode == "match_data":
+    try:
+        kvk_map_id = sys.argv[1]
+        kvk_infos = os.environ["KVK_INFOS"]
+        info_list:dict = json.loads(kvk_infos)
+        show_kvk_match_data(info_list.get(kvk_map_id))
+    except Exception as e:
+        print(str(e))
+        raise e
