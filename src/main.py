a69cb9542d4611719e48b168752c8aa88e702650
son-sai <sonsai.forwork@gmail.com>
2026-03-07 15:00:18 +0900

    添加DKP查询服务

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import json
 import os
 import sys
-from .utility import show_kvk_match_data
+from .utility import show_kvk_match_data, show_kvk_dkp
 
 mode = os.environ["MODE"]
 
@@ -13,6 +13,18 @@ if mode == "match_data":
         print("kvk_map_id:" + str(kvk_map_id))
         print("info_list:" + str(info_list))
         show_kvk_match_data(info_list.get(kvk_map_id))
+    except Exception as e:
+        print(str(e))
+        raise e
+
+elif mode == "dkp_data":
+    try:
+        kvk_map_id = sys.argv[1]
+        kvk_infos = os.environ["KVK_INFOS"]
+        info_list:dict = json.loads(kvk_infos)
+        print("kvk_map_id:" + str(kvk_map_id))
+        print("info_list:" + str(info_list))
+        show_kvk_dkp(info_list.get(kvk_map_id))
     except Exception as e:
         print(str(e))
         raise e
