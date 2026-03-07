8432c6d504f42fb59b188e28c27350a00436efb2
son-sai <sonsai.forwork@gmail.com>
2026-03-07 14:24:58 +0900

    bug对应

--- src/utility.py
+++ src/utility.py
@@ -15,7 +15,7 @@ def fn(n):
 def total_kingdom(data_list):
     dkp_t4_dead = os.environ["DKP_T4_DEAD"]
     dkp_t5_dead = os.environ["DKP_T5_DEAD"]
-    print(f"统计起始日:{data_list[0].get("from_date")}，统计结束日:{data_list[0].get("to_date")}")
+    print(f'统计起始日:{data_list[0].get("from_date")}，统计结束日:{data_list[0].get("to_date")}')
     group_total_kill = 0
     group_total_dead_t4 = 0
     group_total_dead_t5 = 0
@@ -30,18 +30,18 @@ def total_kingdom(data_list):
             total_dead_t4 += p.get("dead_t4", 0)
             total_dead_t5 += p.get("dead_t5", 0)
         # 输出结果
-        print(f"王国:{d.get("kingdom")}",end=" ")
-        print(f"总Kill: {total_kill/100000000:.1f} 亿",end=" ")
-        print(f"总dead_t4: {total_dead_t4/10000:.1f} 万",end=" ")
-        print(f"总dead_t5: {total_dead_t5/10000:.1f} 万")
+        print(f'王国:{d.get("kingdom")}',end=" ")
+        print(f'总Kill: {total_kill/100000000:.1f} 亿',end=" ")
+        print(f'总dead_t4: {total_dead_t4/10000:.1f} 万',end=" ")
+        print(f'总dead_t5: {total_dead_t5/10000:.1f} 万')
         group_total_kill += total_kill
         group_total_dead_t4 += total_dead_t4
         group_total_dead_t5 += total_dead_t5
     # 输出结果
-    print(f"阵营总Kill: {group_total_kill/100000000:.1f} 亿",end=" ")
-    print(f"dead_t4: {group_total_dead_t4/10000:.1f} 万",end=" ")
-    print(f"dead_t5: {group_total_dead_t5/10000:.1f} 万")
-    print(f"阵营总DKP: {(group_total_kill+group_total_dead_t4*dkp_t4_dead+group_total_dead_t5*dkp_t5_dead)/1000000000:.1f} B")
+    print(f'阵营总Kill: {group_total_kill/100000000:.1f} 亿',end=" ")
+    print(f'dead_t4: {group_total_dead_t4/10000:.1f} 万',end=" ")
+    print(f'dead_t5: {group_total_dead_t5/10000:.1f} 万')
+    print(f'阵营总DKP: {(group_total_kill+group_total_dead_t4*dkp_t4_dead+group_total_dead_t5*dkp_t5_dead)/1000000000:.1f} B')
 
 def show_kvk_match_data(
         kvk_info,show_kingdom:bool=True, 
@@ -50,10 +50,10 @@ def show_kvk_match_data(
 
     camps:dict = kvk_info.get("camps")
     print("-------KVK MATCH DATAS-------")
-    print(f"MAP:{kvk_info.get("kvk_map_id", "Unknown")}")
+    print(f'MAP:{kvk_info.get("kvk_map_id", "Unknown")}')
     for key in camps.keys():
         kingdoms = camps.get(key)
-        print(f"CAMP:{key} {kingdoms}")
+        print(f'CAMP:{key} {kingdoms}')
         total_dead = 0
         total_kill = 0
         total_power = 0
@@ -65,13 +65,13 @@ def show_kvk_match_data(
             power = response["data"]["power"]
             kvk_score = response["data"]["kvkKillScore"]
             if show_kingdom:
-                print(f"KD:{k},KVK-SCORE:{fn(kvk_score)},POWER:{fn(power)},DEAD:{fn(dead)},KILL:{fn(kill)}")
+                print(f'KD:{k},KVK-SCORE:{fn(kvk_score)},POWER:{fn(power)},DEAD:{fn(dead)},KILL:{fn(kill)}')
             total_dead += dead
             total_kill += kill
             total_power += power
             total_score += kvk_score
         if show_sum:
-            print(f"TOTAL-KVK-SCORE:{fn(total_score)},TOTAL-POWER:{fn(total_power)},TOTAL-DEAD:{fn(total_dead)},TOTAL-KILL:{fn(total_kill)}")
+            print(f'TOTAL-KVK-SCORE:{fn(total_score)},TOTAL-POWER:{fn(total_power)},TOTAL-DEAD:{fn(total_dead)},TOTAL-KILL:{fn(total_kill)}')
 
 def show_kvk_dkp(kvk_info):
     print("-------KVK DKP-------")
@@ -79,10 +79,10 @@ def show_kvk_dkp(kvk_info):
     end = kvk_info.get("end")
     camps:dict = kvk_info.get("camps")
     
-    print(f"MAP:{kvk_info.get("kvk_map_id", "Unknown")}")
+    print(f'MAP:{kvk_info.get("kvk_map_id", "Unknown")}')
     for key in camps.keys():
         kingdoms = camps.get(key)
-        print(f"CAMP:{key}")
+        print(f'CAMP:{key}')
         data_list = []
         for k in kingdoms:
             response = get_listed_kingdoms_member_info_api(
